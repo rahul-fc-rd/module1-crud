0cc5c377789f83ff6984eda2f86abadecbae9418
Rahul Kumar <rahoul1804@gmail.com>
2021-07-18 21:59:48 +0530

    ui crossorigin added and port chnages as per i=ui

--- src/main/java/com/fc/rd/module1/crud/controller/UserController.java
+++ src/main/java/com/fc/rd/module1/crud/controller/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,6 +26,7 @@ import com.fc.rd.module1.crud.util.ResponseBean;
 
 @RestController
 @RequestMapping(value = "/users")
+@CrossOrigin
 public class UserController {
 
 	@Autowired
